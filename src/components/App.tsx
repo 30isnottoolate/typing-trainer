5604f2825ac8225c7e742aa41036705b004a9f8c
30isnottoolate <akos.varga.92@gmail.com>
2022-12-21 17:09:00 +0100

    Add basic elements

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,9 +3,10 @@ import React from "react";
 const App: React.FC = () => {
     return (
         <div>
-            So it begins...
+            <div id="text-source">Some text.</div>
+            <textarea id="text-input" />
         </div>
-    )
+    );
 }
 
 export default App;
