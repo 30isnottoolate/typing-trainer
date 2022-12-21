2751b1f7ae80bf3d947067ef3abe3c7b2419c111
30isnottoolate <akos.varga.92@gmail.com>
2022-12-21 18:40:03 +0100

    Add time counting functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,16 +4,24 @@ const App: React.FC = () => {
     const [textSource, setTextSource] = useState("Some text.");
     const [textInput, setTextInput] = useState("");
     const [errorCount, setErrorCount] = useState(0);
+    const [timerActive, setTimerActive] = useState(false);
+    const [startingTime, setStartingTime] = useState(0);
 
     const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         if (event.target) {
             setTextInput(event.currentTarget.value);
+
+            if (!timerActive) {
+                setTimerActive(true);
+                setStartingTime(Date.now());
+            }
 
             if (event.currentTarget.value !== textSource.slice(0, event.currentTarget.value.length)) {
                 setErrorCount(prevState => prevState + 1);
             } else if (event.currentTarget.value === textSource) {
                 console.log("Done. Number of errors: " + errorCount);
                 console.log(`Accuray: ${(100 - errorCount / textSource.length * 100)}%`);
+                console.log(`Time: ${Date.now() - startingTime}`);
             }
         }
     }
