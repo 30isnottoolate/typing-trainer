f7ab389ac8d1b47d2c3e2ba326f8f58bccf24f45
30isnottoolate <akos.varga.92@gmail.com>
2022-12-21 17:28:50 +0100

    Add basic error checking

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,10 +4,20 @@ const App: React.FC = () => {
     const [textSource, setTextSource] = useState("Some text.");
     const [textInput, setTextInput] = useState("");
 
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if (event.target) {
+            setTextInput(event.currentTarget.value);
+
+            if (event.currentTarget.value !== textSource.slice(0, event.currentTarget.value.length)) {
+                console.log("wrong!");
+            }
+        }
+    }
+
     return (
         <div>
             <div id="text-source">{textSource}</div>
-            <textarea id="text-input" value={textInput} onChange={(event) => setTextInput(event.currentTarget.value)} />
+            <textarea id="text-input" value={textInput} onChange={(event) => handleChange(event)} />
         </div>
     );
 }
