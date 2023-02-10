ed4fef30868407fa277385eb6a1f60ec0cd4fc63
30isnottoolate <akos.varga.92@gmail.com>
2023-02-10 18:37:58 +0100

    Add success prop to component and implement it

--- src/components/FinishBox.tsx
+++ src/components/FinishBox.tsx
@@ -4,13 +4,15 @@ interface FinishBoxProps {
     finishTime: number;
     finishAccurary: number;
     finishSpeed: number;
+    success: boolean;
 }
 
-const FinishBox: React.FC<FinishBoxProps> = ({ finishTime, finishAccurary, finishSpeed }: FinishBoxProps) => {
+const FinishBox: React.FC<FinishBoxProps> = ({ finishTime, finishAccurary, finishSpeed, success }: FinishBoxProps) => {
 
     return (
         <div className="menu-screen">
             <div className="finish-box">
+                <p>{success ? "Success! Next level unlocked." : "You need more practice to advance."}</p>
                 <div className="finish-stats-container">
                     <span>Time: </span><span>{finishTime}</span>
                     <span>Accuracy: </span><span>{finishAccurary}</span>
