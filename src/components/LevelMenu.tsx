5efff1733991a9cda90b9f90f230518604785c58
30isnottoolate <akos.varga.92@gmail.com>
2023-02-10 17:33:15 +0100

    Add basic elements to menu

--- src/components/LevelMenu.tsx
+++ src/components/LevelMenu.tsx
@@ -8,7 +8,13 @@ const LevelMenu: React.FC<LevelMenuProps> = ({ }: LevelMenuProps) => {
     return (
         <div className="menu-screen">
             <div className="menu-box">
-
+                <label htmlFor="level-selection">Choose a level:</label>
+                <select name="level-selection">
+                    <option>1</option>
+                    <option disabled>2</option>
+                    <option disabled>3</option>
+                </select>
+                <button>Start Training</button>
             </div>
         </div>
     );
