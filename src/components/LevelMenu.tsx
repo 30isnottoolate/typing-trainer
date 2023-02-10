b5eb512513576f94d05c3fc7035195b59e214576
30isnottoolate <akos.varga.92@gmail.com>
2023-02-10 17:21:31 +0100

    Create new component

---
+++ src/components/LevelMenu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface LevelMenuProps {
+}
+
+const LevelMenu: React.FC<LevelMenuProps> = ({ }: LevelMenuProps) => {
+
+    return (
+        <div className="menu-screen">
+            <div className="menu-box">
+
+            </div>
+        </div>
+    );
+}
+
+export default LevelMenu;
