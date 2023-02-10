656186e44b17649f905df572eb206a185eadcda2
30isnottoolate <akos.varga.92@gmail.com>
2023-02-10 18:44:47 +0100

    Remove duplicate code

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { FIRST_ROW, SECOND_ROW, THIRD_ROW, FOURTH_ROW, FIFTH_ROW, COLUMN_CLASSES } from "../utilities/columnsAndRows";
 
-/* const FIRST_ROW = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "", "", ""];
-const SECOND_ROW = ["", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "", "", ""];
-const THIRD_ROW = ["", "a", "s", "d", "f", "g", "h", "j", "k", "l", "", "", ""];
-const FOURTH_ROW = ["", "z", "x", "c", "v", "b", "n", "m", ",", ".", "", ""];
-const FIFTH_ROW = ["", "", "", " ", "", "", "", ""];
-
-const COLUMN_CLASSES = ["", "first", "second", "third", "fourth", "fourth", "first", "first", "second", "third", "fourth"]; */
-
 interface KeyboardProps {
     currentKey: string;
 }
