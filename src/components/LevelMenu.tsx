cf564f484065374b8122260c5a554779900d5283
30isnottoolate <akos.varga.92@gmail.com>
2023-02-10 17:55:31 +0100

    Add more elements to menu

--- src/components/LevelMenu.tsx
+++ src/components/LevelMenu.tsx
@@ -9,6 +9,19 @@ const LevelMenu: React.FC<LevelMenuProps> = ({ }: LevelMenuProps) => {
     return (
         <div className="menu-screen">
             <div className="menu-box">
+                <p>Highest level reached:</p>
+                <p>Settings</p>
+                <p>Threshold to unlock next level:</p>
+                <div>
+                    <label htmlFor="accuracy">Accuracy:</label>
+                    <input type="number" name="accuracy" min="75" max="100" value="85" />
+                    <span>%</span>
+                </div>
+                <div>
+                    <label htmlFor="speed">Speed:</label>
+                    <input type="number" name="speed" min="150" max="300" value="180" />
+                    <span>char/min</span>
+                </div>
                 <label htmlFor="level-selection">Choose a level:</label>
                 <select name="level-selection">
                     <option>1</option>
