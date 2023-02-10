d2ce7d45ae0bb35cdff6c8b572b2599805d45ca5
30isnottoolate <akos.varga.92@gmail.com>
2023-02-10 18:29:48 +0100

    Create new component

---
+++ src/components/FinishBox.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FinishBox: React.FC = () => {
+
+    return (
+        <div className="menu-screen">
+            <div className="finish-box">
+                <button>Next</button>
+                <button>Restart</button>
+                <button>Menu</button>
+            </div>
+        </div>
+    );
+}
+
+export default FinishBox;
