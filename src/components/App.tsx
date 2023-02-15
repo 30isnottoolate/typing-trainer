33bdca5f5d56512173f4158cf4e0835a30eddb40
30isnottoolate <akos.varga.92@gmail.com>
2023-02-15 15:20:25 +0100

    Remove unnecessary code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,46 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import LevelMenu from "./LevelMenu";
 import Trainer from "./Trainer";
 
-import wordBank from "../utilities/wordBank";
-
 const App: React.FC = () => {
     const [textInput, setTextInput] = useState("");
     const [currentLevel, setCurrentLevel] = useState(1);
-    const [appStatus, setAppStatus] = useState("menu"); //menu, training, paused, finished
+    const [appStatus, setAppStatus] = useState("menu"); // menu, training
     const [progressionScore, setProgressionScore] = useState({ accuracy: 95, speed: 180 });
-
-    /* useEffect(() => {
-        if (currentLevel > 0) {
-            let currentWords = wordBank[currentLevel - 1];
-            let textCollector = "";
-
-            for (let numberOfLines = 0; numberOfLines < 3; numberOfLines++) {
-                let lineCollector = "";
-                let isLineFull = false;
-
-                do {
-                    let randomIndex = Math.floor(Math.random() * currentWords.length);
-                    let randomWord = currentWords[randomIndex];
-
-                    if ((lineCollector + randomWord).length < 66) {
-                        lineCollector = `${lineCollector} ${randomWord}`;
-                    } else {
-                        textCollector = `${textCollector}\n${lineCollector.trim()}`;
-                        isLineFull = true;
-                    }
-                }
-                while (!isLineFull);
-            }
-
-            setTextSource(textCollector.trim());
-        }
-    }, [currentLevel]); */
-
-    /* useEffect(() => {
-        if (appStatus === "training" && textAreaRef.current) textAreaRef.current.focus();
-    }, [appStatus]) */
 
     return (
         <>
