64748417ca94883302852c0be1ef4d91ed325c22
30isnottoolate <akos.varga.92@gmail.com>
2023-02-17 15:33:27 +0100

    Create textGenerator function in an external file

---
+++ src/utilities/textGenerator.tsx
@@ -0,0 +1,30 @@
+const textGenerator = (currentLevel: number, wordBank: [string[]]) => {
+    let textCollector = "";
+
+    if (currentLevel > 0) {
+        let currentWords = wordBank[currentLevel - 1];
+        
+
+        for (let numberOfLines = 0; numberOfLines < 3; numberOfLines++) {
+            let lineCollector = "";
+            let isLineFull = false;
+
+            do {
+                let randomIndex = Math.floor(Math.random() * currentWords.length);
+                let randomWord = currentWords[randomIndex];
+
+                if ((lineCollector + randomWord).length < 66) {
+                    lineCollector = `${lineCollector} ${randomWord}`;
+                } else {
+                    textCollector = `${textCollector}\n${lineCollector.trim()}`;
+                    isLineFull = true;
+                }
+            }
+            while (!isLineFull);
+        }
+    }
+
+    return textCollector.trim();
+}
+
+export default textGenerator;
