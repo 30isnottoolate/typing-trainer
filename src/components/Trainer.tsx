ba2b2b0dd57ad93194772051b70cda4e9a9d63b1
30isnottoolate <akos.varga.92@gmail.com>
2023-02-18 13:18:00 +0100

    Add icon to pause-button

--- src/components/Trainer.tsx
+++ src/components/Trainer.tsx
@@ -135,7 +135,13 @@ const Trainer: React.FC<TrainerProps> = (
                     />
                     <Keyboard currentKey={currentKey} />
                     <div id="pause-button" onClick={() => pauseTraining()}>
-                        II
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="2rem"
+                            height="2rem"
+                            viewBox="0 0 16 16">
+                            <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
                     </div>
                 </>
             }
