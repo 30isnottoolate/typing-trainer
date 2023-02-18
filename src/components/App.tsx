1c4074673527ca3b0cfd64d1576766d622eaa4bf
30isnottoolate <akos.varga.92@gmail.com>
2023-02-18 11:03:39 +0100

    Add ability to store progScore in localStorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import LevelMenu from "./LevelMenu";
 import Trainer from "./Trainer";
@@ -6,7 +6,24 @@ import Trainer from "./Trainer";
 const App: React.FC = () => {
     const [currentLevel, setCurrentLevel] = useState(1);
     const [appStatus, setAppStatus] = useState("menu"); // menu, training
-    const [progressionScore, setProgressionScore] = useState({ accuracy: 95, speed: 180 });
+    const [progressionScore, setProgressionScore] = useState(() => {
+        if (localStorage.accuracy && Number(localStorage.accuracy) >= 75 && Number(localStorage.accuracy) <= 100 &&
+            localStorage.speed && Number(localStorage.speed) >= 150 && Number(localStorage.speed) <= 300) {
+
+            return { accuracy: Number(localStorage.accuracy), speed: Number(localStorage.speed) }
+        } else {
+            localStorage.accuracy = 95;
+            localStorage.speed = 180;
+
+            return { accuracy: 95, speed: 180 }
+        }
+    });
+
+    useEffect(() => {
+        localStorage.accuracy = progressionScore.accuracy;
+        localStorage.speed = progressionScore.speed;
+        
+    }, [progressionScore.accuracy, progressionScore.speed])
 
     return (
         <>
