eb85fb4e71601fbd6a821122ccce5baff227d335
30isnottoolate <akos.varga.92@gmail.com>
2023-02-20 16:25:30 +0100

    Add conditional finishMessage to component

--- src/components/FinishBox.tsx
+++ src/components/FinishBox.tsx
@@ -1,27 +1,33 @@
-import React, {Dispatch, SetStateAction} from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 interface FinishBoxProps {
     currentLevel: number;
-    score: {time: number, accuracy: number, speed: number, success: boolean};
+    score: { time: number, accuracy: number, speed: number, success: boolean };
     setAppStatus: Dispatch<SetStateAction<string>>;
     nextLevel: () => void;
     restartTraining: () => void;
 }
 
+const MAX_LEVEL = 10;
+
 const FinishBox: React.FC<FinishBoxProps> = (
     { currentLevel, score, setAppStatus, nextLevel, restartTraining }: FinishBoxProps) => {
+
+    const finishMessage = score.success && currentLevel < MAX_LEVEL ? "Success! Next level unlocked."
+        : score.success && currentLevel === MAX_LEVEL ? "Success! Training completed."
+            : "You need more practice to advance.";
 
     return (
         <div className="menu-screen">
             <div className="finish-box">
-                <p>{score.success ? "Success! Next level unlocked." : "You need more practice to advance."}</p>
+                <p>{finishMessage}</p>
                 <div className="finish-stats-container">
                     <span>Time: </span><span>{score.time} s</span>
                     <span>Accuracy: </span><span>{score.accuracy} %</span>
                     <span>Speed: </span><span>{score.speed} chars/min</span>
                 </div>
                 <div className="buttons-container">
-                    {score.success &&
+                    {(score.success && currentLevel < MAX_LEVEL) &&
                         <button onClick={nextLevel}>Next</button>
                     }
                     <button onClick={restartTraining}>Restart</button>
